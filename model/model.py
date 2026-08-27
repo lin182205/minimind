bf65385fb38fde6def38eee747978c011e9868f3
Linwb <151309662@qq.com>
2026-08-28 03:03:14 +0800

    rope

--- model/model.py
+++ model/model.py
@@ -1,3 +1,5 @@
+from transformers import PreTrainedModel, GenerationMixin, PretrainedConfig
+
 class MiniMindConfig(PretrainedConfig):
     model_type = "minimind"
     def __init__(self, hidden_size=768, num_hidden_layers=8, use_moe=False, **kwargs):
@@ -34,3 +36,77 @@ class MiniMindConfig(PretrainedConfig):
         self.moe_intermediate_size = kwargs.get("moe_intermediate_size", self.intermediate_size)
         self.norm_topk_prob = kwargs.get("norm_topk_prob", True)
         self.router_aux_loss_coef = kwargs.get("router_aux_loss_coef", 5e-4)
+
+# 继承nn.module
+#初始化
+#
+import torch 
+import torch.nn as nn
+import math
+
+class RMSNorm(nn.module):
+    def __init__(self, dim: int, eps: float = 1e-5):
+        super().__(init)__()
+        self.eps = eps
+        self.weight = nn.Parameter(torch.ones(dim))
+
+    def norm(self, x):
+        return x*torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
+
+    def forward(self, x):
+        return (self.weight * self.norm(x.float())).type_as(x)
+
+def Precompute_freqs_cis(dim: int, end: int = 32*1024, rope_base: float = 1e6, rope_scaling: dict = None):
+    freqs, attn_factor = 1.0 / (rope_base ** (torch.arrange(0, dim, 2)[: (dim // 2)].float() / dim)), 1.0
+    if rope_scaling is not None: #YaRN
+        beta_fast, beta_slow, factor, origin_max, attn_factor = (
+            rope_scaling.get("beta_fast", 32.0), rope_scaling.get("beta_slow", 1.0), 
+            rope_scaling.get("factor", 16),
+            rope_scaling.get("oringinal_max_position_embeddings", 2048), 
+            rope_scaling.get("attention_factor", 1.0)
+        )
+        if end > origin_max:
+            inv_dim = lambda b: (dim * math.log(origin_max / (b * 2 * math.pi))) / (2 * math.log(rope_base))
+            low, high = max(math.floor(inv_dim(beta_fast)), 0), min(math.ceil(inv_dim(beta_slow)), dim // 2 - 1)
+            ramp =  torch.clamp((torch.arrange(0, dim//2, device = freqs.device).float() - low) / max(high - low, 0.001), 0, 1)
+            freqs = freqs * (1- ramp + ramp / factor)
+    t = torch.arrange(end, device = freqs.device)
+    freqs = torch.outer(t, freqs).float()
+    freqs_cos = torch.cat([torch.cos(freqs), torch.cos(freqs)], dim = -1) * attn_factor
+    freqs_sin = torch.cat([torch.sin(freqs), torch.sin(freqs)], dim = -1) * attn_factor
+    return freqs_cos, freqs_sin
+
+def apply_rotary_pos_emb(q, k, cos, sin, unsqueeze_dim = 1):
+    def rotate_half(x):
+        return torch.cat((-x[..., x.shape[-1] // 2: ],x[..., :x.shape[-1] // 2]), dim = -1)
+    q_embed = ((q * cos.unsqueeze(unsqueeze_dim)) + (rotate_half(q) * sin.unsqueeze(unsqueeze_dim))).to(q.dtype)
+    k_embed = ((k * cos.unsqueeze(unsqueeze_dim)) + (rotate_half(k) * sin.unsqueeze(unsqueeze_dim))).to(k.dtype)
+    return q_embed, k_embed
+
+def repeat_kv(x: torch.Tensor, n_rep: int) -> torch.Tensor: 
+    batchsize, slen, num_key_value_heads, head_dim = x.shape
+    if n_rep == 1: return x
+    return x.unsqueeze(3).expand(batchsize, slen, num_key_value_heads, n_rep, head_dim).reshape(batchsize, slen, num_key_value_heads * n_rep, head_dim)
+
+class Attention(nn.Module):
+    def __init__(self, config: MiniMindConfig):
+        super().__init__()
+        self.num_key_value_heads = config.num_attention_heads if config.num_key_value_heads is None else config.num_key_value_heads
+        self.n_local_heads = config.num_attention_heads
+        self.n_local_kv_heads = self.num_key_value_heads
+        self.n_rep = self.n_local_heads // self.n_local_kv_heads
+        self.head_dim = config.head_dim
+        self.is_causal = True
+        self.q_proj = nn.Linear(config.hidden_size, config.num_attention_heads * self.head_dim, bias=False)
+        self.k_proj = nn.Linear(config.hidden_size, self.num_key_value_heads * self.head_dim, bias=False)
+        self.v_proj = nn.Linear(config.hidden_size, self.num_key_value_heads * self.head_dim, bias=False)
+        self.o_proj = nn.Linear(config.num_attention_heads * self.head_dim, config.hidden_size, bias=False)
+        self.q_norm = RMSNorm(self.head_dim, eps=config.rms_norm_eps)
+        self.k_norm = RMSNorm(self.head_dim, eps=config.rms_norm_eps)
+        self.attn_dropout = nn.Dropout(config.dropout)
+        self.resid_dropout = nn.Dropout(config.dropout)
+        self.dropout = config.dropout
+        self.flash = hasattr(torch.nn.functional, 'scaled_dot_product_attention') and config.flash_attn
+
+    def forward()
+
